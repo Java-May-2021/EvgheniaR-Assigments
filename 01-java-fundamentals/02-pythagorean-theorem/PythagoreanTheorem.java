0f9515b41d505cb5d77e95de909b832cfa442921
Evghenia Raileanu <cantomd@mail.ru>
2021-05-03 21:00:32 -0700

    Pythagorean Theorem

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTheorem.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.lang.Math;
+public class PythagoreanTheorem{
+    public static double calculateHypotenuse (int lenA, int lenB) {
+        return Math.sqrt((lenA * lenA) + (lenB * lenB));
+    }
+    
+}
